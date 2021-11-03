b8ef42c0d0daffcb5918ef12fbb373098889f93e
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-03 16:35:57 -0500

    Se definieron las estructuras basicas del simulador

---
+++ Simulation/src/model/Espacios.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author USER
+ */
+class Espacios {
+    
+    
+    
+    //Parametros para calcular medidas del espacio
+   
+    
+    private float ancho_de_espacio;
+    private float largo_de_espacio;
+    private float altura_de_espacio;
+    
+    
+    
+    
+    //Area de trabajo
+    private float altura_de_trabajo;
+    
+    
+    
+    //Cantidad de lamparas en el espacio
+    private int cantidad_de_lamparas;
+    
+    
+    
+    //Desplazamiento de las lamparas entre sí
+    private float desplazamiento_de_lamparas;
+    
+    
+}
