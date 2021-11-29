9a79d84ba04c10d3d656dbaa0bf8319983083ae6
NilsonVargas <59322799+NilsonVargas@users.noreply.github.com>
2021-11-29 14:12:48 -0500

    Se implemento metodo bajo iluminacion indirecta

--- Simulation/src/model/SistemaIluminacion.java
+++ Simulation/src/model/SistemaIluminacion.java
@@ -16,11 +16,12 @@ public class SistemaIluminacion
     
     
     
-    public float obtenerIndiceLocal(float ancho,float largo,float altura){
+    public float obtenerIndiceLocal(float ancho,float largo,float altura, float plano_trabajo){
         float indice_local = 0;
         switch(this.value)
         {
             case 0://Si el tipo de iluminacion es indirecta
+                indice_local  = this.iluminacionIndirecta(ancho, largo, altura, plano_trabajo);
                 break;
             
             case 1://Si el tipo de iluminacion es directa
@@ -42,10 +43,11 @@ public class SistemaIluminacion
         return indice_local_iluminacion_directa;
     }
     
-    public float iluminacionIndirecta(float ancho,float largo,float altura)
+    public float iluminacionIndirecta(float ancho,float largo,float altura, float plano_trabajos)
     {
         float indice_local_iluminacion_indirecta = 0;
         
+        indice_local_iluminacion_indirecta = (3*ancho*largo)/(2*(altura+plano_trabajos)*(ancho*largo));
         
         return indice_local_iluminacion_indirecta;
     }
